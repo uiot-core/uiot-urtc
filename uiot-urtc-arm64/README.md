cf9ca67608f817ba5a26d70c445909f3ea9bac76
Your Name <you@example.com>
2020-02-16 17:52:26 +0800

    commit arm64

---
+++ uiot-urtc-arm64/README.md
@@ -0,0 +1,34 @@
+# URTC ARM64 SDK DEMO
+
+适用环境：ubuntu 16.04 18.04
+编译工具: cmake make gcc g++
+
+URTC 以动态链接库的方式提供SDK，包括两个文件：
+- urtclib/libliburtcmediaengine.so
+- urtclib/liburtcnetengine.so
+
+URTC DEMO为开源，用户可以直接使用到自己的产品中，本demo使用方法：
+
+```
+cd build
+# 生成Makefile等
+cmake ../.
+# 编译成功后，在../bin中生成可执行文件
+make
+# 执行
+cd ..
+bin/enginedemo rtsp://path/to/rtspstream
+```
+
+**注意事项**
+1. 使用本库需要了解URTC的基本概念，包括应用名称，AppID, AppKey, roomid，userid, tokenid, 具体使用方法请参考[URTC文档](https://docs.ucloud.cn/video/urtc)
+2. URTC在物联网场景下:
+   - AppID为系统自动生成;
+   - 【重要】AppKey为接入平台认证Key，本demo中直接使用该Key接入，生产环境下务必将AppKey存在服务器端,[使用Token接入](https://docs.ucloud.cn/video/urtc/sdk/token)；
+   - 应用名称采用 产品序列号+产品名称，比如：ozuz63kum2i4djb3_巡检无人机；
+   - roomid采用设备序列号，比如：afnyhnizq9l4l9ev；
+   - userid采用设备序列号+摄像头编号，比如：afnyhnizq9l4l9ev_camera3；
+   - tokenid为服务器端生成，测试环境直接使用AppKey测试，生产环境务必参考本项第2条；
+
+3. 生产模式使用Tokenid，配置`URTCConfig.cpp`，`mSdkMode = UCLOUD_RTC_SDK_MODE_NORMAL;`, 此时在`main.cpp	中获取tokenid;
+   测试环境可以暂使用（不安全）AppKey，配置`URTCConfig.cpp`，`mSdkMode = UCLOUD_RTC_SDK_MODE_TRIVAL; mSeckey = "xxxxxxxxxxxxxxxxxxxxxx";`
---
+++ uiot-urtc-arm64/main.cpp
@@ -0,0 +1,64 @@
+#include "URTCConfig.h"
+#include "RTCEngineFactory.h"
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "URTCMsgHandler.h"
+#include "msgqueue.h"
+#include <iostream>
+
+URTCMsgHandler* g_msghandler = nullptr ;
+
+int main(int argc, char **argv)
+{
+	std::string roomid = "afnyhnizq9l4l9ev";
+	std::string userid = "afnyhnizq9l4l9ev_camera3" ;
+	std::string tokenid = "eyJhcHBfaWQiOiJ1cnRjLTRtYjB0dW9yIiwicm9vbV9pZCI6ImFmbnlobml6cTlsNGw5ZXYiLCJ1c2VyX2lkIjoiYWZueWhuaXpxOWw0bDlldl9jYW1lcmEzIn0=.fc4d0c1d202a6f749eb84b1df846dcd06b102a171578648069f0c5341e";
+	std::string cmd = "" ;
+	std::string rtspurl = "" ;
+	
+	if(argc>=2) 
+	{
+		rtspurl = argv[1];
+		std::cout <<"input rtsp url "<< rtspurl <<std::endl ;
+	}
+
+	if(argc>=3) 
+	{
+		roomid = argv[2];
+		std::cout <<"input roomid "<< roomid <<std::endl ;
+	}
+	if(argc>=4) 
+	{
+		userid = argv[3];
+		std::cout <<"input userid "<< userid <<std::endl ;
+	}
+	
+	RTCEngineBase* urtcengine = RTCEngineFactory::createRtcEngine(RTC_CHANNELTYPE_UCLOUD);
+	if (urtcengine)
+	{
+		g_msghandler = new URTCMsgHandler(urtcengine) ;
+		MsgQueue::getInstance()->regEventHandler("", g_msghandler);
+		MsgQueue::getInstance()->startMsgQue() ;
+        	urtcengine->InitRTCEngine("./data/log", 0);
+		urtcengine->EnableRtspSource(1, true, rtspurl) ;
+		g_msghandler->setUserId(roomid) ;
+        	g_msghandler->setRoomId(userid) ;
+
+
+		tRTCAuthInfo auth;
+		auth.mAppid = URTCConfig::getInstance()->getAppId();
+		auth.mRoomid = roomid;
+		auth.mUserid = userid ;
+		auth.mToken = tokenid;
+		urtcengine->JoinRoom(auth);	
+		while (true)
+		{
+			usleep(1000*1000) ;
+		}
+	}
+    return 0;
+}
+
+
